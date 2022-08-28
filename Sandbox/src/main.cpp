fb83605b6a74afa17a742cd0938a3e59434b46cb
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2022-08-28 17:37:08 +0530

    removed application class, need to work on actual engine now, entry method can be decided later

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -2,37 +2,11 @@
 * A game/app that uses the Hart Engine, currently used for testing purpores
 */
 
-#include "Core/HartApplication.hpp"
-
-class Sandbox : public Hart::Core::HartApplication {
-protected:
-	void init() {
-		HART_CLIENT_INFO("init");
-	}
-	void update() {
-		HART_CLIENT_INFO("update");
-	}
-	void render() {
-		HART_CLIENT_INFO("render");
-	}
-	void deinit() {
-		HART_CLIENT_INFO("deinit");
-	}
-};
-
 #include <iostream>
-#include "GLFW/glfw3.h"
 
 int main() {
 
-	Sandbox sandbox;
 
-	sandbox.start();
-	sandbox.run();
-	sandbox.shutdown();
-
-	glfwInit();
-	glfwTerminate();
 
 	std::cin.get();
 	return 0;
