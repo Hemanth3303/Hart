4a71aa8cc90459a5e89160039e1aeb646426b392
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2022-10-07 22:07:33 +0530

    like i said to myself earlier(and then ignored), can deal with engine class after we have an engine :)

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -2,27 +2,22 @@
 * A game/app that uses the Hart Engine, currently used for testing purpores
 */
 
-#include "Core/Engine.hpp"
-#include "Core/Scene.hpp"
+#include "Core/Window.hpp"
 #include "Graphics/Shader.hpp"
 
 int main(int argc, char** argv) {
 
-	Hart::Engine::Init();
-	Hart::Scene scene1;
-	Hart::Scene scene2;
-	
+	Hart::Window window(640, 480, "HART ENGINE: SANDBOX", false);
 	Hart::Graphics::Shader shader("res/shaders/basicVert.glsl", "res/shaders/basicFrag.glsl");
 
-	Hart::Engine::AddScene(scene1, "test_scene_1");
-	Hart::Engine::AddScene(scene2, "test_scene_2");
+	while (!glfwWindowShouldClose(window.getGlfwWindow())) {
+		window.handleEvents();
+		window.update();
 
-	while (Hart::Engine::IsRunning()) {
-		Hart::Engine::HandleEvents();
-		Hart::Engine::Update();
-		Hart::Engine::Render();
+		window.clear();
+		//draw calls
 
-		Hart::Engine::SetAsActiveScene("test_scene_2");
+		window.swapBuffers();
 	}
 	
 	return 0;
