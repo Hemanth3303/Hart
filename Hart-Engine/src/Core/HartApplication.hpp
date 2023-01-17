68b5a055dc9927eac7f88857f3b14071eed87410
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2023-01-17 22:48:43 +0530

    changed the way in which window is configured

--- Hart-Engine/src/Core/HartApplication.hpp
+++ Hart-Engine/src/Core/HartApplication.hpp
@@ -15,20 +15,21 @@ namespace Hart {
 	class Application {
 	public:
 		Application();
+		// takes window configurations as arguments
+		Application(int32_t windowWidth, int32_t windowHeight, const std::string& windowTitle, bool isWindowResizable);
 		virtual ~Application();
 
 		void run();
-		// set the configurations with which the window is created.
-		// has not effect if called outside the constructor of a child class of the application class
-		void setWindowConfigs(int32_t width, int32_t height, const std::string& title, bool resizable);
 
 		inline const int32_t getWindowWidth() const { return m_Window->getWidth(); }
 		inline const int32_t getWindowHeight() const { return m_Window->getHeight(); }
 		inline const Maths::Vec2& getMousePositon() const { return m_Window->getMousePosition(); }
 		inline void setMousePosition(const Maths::Vec2& position) const { m_Window->setMousePosition(position); }
-		// sets the preffered frames per second
+		// sets the preferred frames per second
+		// default value is 60
 		inline void setTargetFPS(uint32_t targetFPS) { m_TargetFPS = targetFPS; }
-		// sets the preffered updates per second
+		// sets the preferred updates per second
+		// default value is 60
 		inline void setTargetUPS(uint32_t targetUPS) { m_TargetUPS = targetUPS; }
 		inline uint32_t getCurrentFPS() { return m_CurrentFPS; }
 		inline uint32_t getCurrentUPS() { return m_CurrentUPS; }
@@ -43,6 +44,8 @@ namespace Hart {
 		void init();
 		void deinit();
 		void handleEvents();
+		// set the configurations with which the window is created.
+		void setWindowConfigs(int32_t width, int32_t height, const std::string& title, bool resizable);
 	private:
 		std::unique_ptr<Window> m_Window;
 		WindowConfigs m_Configs;
