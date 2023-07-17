47b922f2650887b09047f18d71f1a2215ba2be0f
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2023-07-17 15:36:55 +0530

    fixed minor bug in layerstack

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -13,6 +13,52 @@ using namespace Hart::Events;
 using namespace Hart::Inputs;
 using namespace Hart::Utils;
 
+class TestLayer : public Layer {
+private:
+	std::shared_ptr<Texture2D> m_GrassTex, m_EmojiTex;
+	OrthographicCameraController m_CameraController;
+	Random m_Rd;
+public:
+	TestLayer(const std::string& name = "TestLayer")
+		:Layer(name), m_CameraController(960.0f, 540.0f) {
+
+		m_GrassTex = std::make_shared<Texture2D>("res/images/grass_block.png", TextureMagFilter::Nearest);
+		m_EmojiTex = std::make_shared<Texture2D>("res/images/awesomeface.png", TextureMagFilter::Linear);
+	}
+
+	~TestLayer() {
+
+	}
+
+	void onAttach() override {
+		HART_CLIENT_LOG(std::string("Attached layer: ") + getName());
+	}
+
+	void onDetach() override {
+		HART_CLIENT_LOG(std::string("Detached layer: ") + getName());
+	}
+
+	void onEvent(Event& e) override {
+		//HART_CLIENT_LOG(e);
+		m_CameraController.onEvent(e);
+	}
+
+	void update(const float deltaTime) override {
+		//HART_CLIENT_LOG("DeltaTime: " + std::to_string(deltaTime) + " | FPS: " + std::to_string(1.0f / deltaTime));
+		m_CameraController.update(deltaTime);
+	}
+
+	void render() override {
+		Renderer2D::BeginScene(m_CameraController.getCamera());
+		
+		Renderer2D::DrawQuad({ 0, 0, 1 }, { 300, 300 }, Gray);
+
+		//HART_CLIENT_TRACE("No of quads: " + std::to_string(count));
+		//HART_CLIENT_TRACE("No of drawcalls: " + std::to_string(Renderer2D::GetNumberOfDrawCalls()));
+		Renderer2D::EndScene();
+	}
+};
+
 class Sandbox : public Application {
 private:
 	std::shared_ptr<Layer3D> m_Layer3D;
@@ -26,10 +72,11 @@ public:
 
 		RenderCommand::SetClearColor(DarkGreenishBlue);
 
-		//m_Layer3D = std::make_shared<Layer3D>("TestLayer3D");
-		m_Layer2D = std::make_shared<Layer2D>("TestLayer2D");
+		//m_Layer3D = std::make_shared<Layer3D>("Layer3D");
+		m_Layer2D = std::make_shared<Layer2D>("Layer2D");
 		//pushLayer(m_Layer3D);
 		pushLayer(m_Layer2D);
+		pushOverlay(std::make_shared<TestLayer>("TestLayer"));
 	}
 
 	~Sandbox() {
