37e822595686443a39994f73db1340961fbd8a62
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2024-01-30 00:16:46 +0530

    removed premake

--- premake5.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set PREMAKE_PATH=.\vendor\premake\.premake\
-
-.\vendor\premake\premake5.exe %1 %2
-
-@echo on
--- premake5.sh
+++
@@ -1,3 +0,0 @@
-export PREMAKE_PATH="./vendor/premake/.premake/"
-
-./vendor/premake/premake5 $1 $2
