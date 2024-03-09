c1968bd8346c91543fcd471b18e9888f4cd97a7a
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2024-03-09 16:05:17 +0530

    removed layer3d

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -5,12 +5,10 @@
 #include "Core/EntryPoint.hpp"
 #include "Hart.hpp"
 #include "Layer2D.hpp"
-#include "Layer3D.hpp"
 
 class Sandbox : public Hart::Application {
 private:
 	std::shared_ptr<Layer2D> m_Layer2D;
-	std::shared_ptr<Layer3D> m_Layer3D;
 public:
 	Sandbox()
 		: Application(960, 540, "Hart Engine: Sandbox", true) {
@@ -21,10 +19,8 @@ public:
 		setBackgroundColor(Hart::DarkGreenishBlue);
 
 		m_Layer2D = std::make_shared<Layer2D>("Layer2D");
-		m_Layer3D = std::make_shared<Layer3D>("Layer3D");
 
 		pushLayer(m_Layer2D);
-		//pushLayer(m_Layer3D);
 	}
 
 	~Sandbox() {
