7a4e01410985f5b8f7940bde5989603a5df87074
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2024-12-27 13:57:35 +0530

    finally finished the example pong game

--- Examples/Two-Player-Pong/src/Two-Player-Pong.cpp
+++ Examples/Two-Player-Pong/src/Two-Player-Pong.cpp
@@ -1,11 +1,17 @@
 // Example Game: Two player controlled pong game
 
+#if defined(HART_WINDOWS)
+	#ifdef DrawText
+		#undef DrawText
+	#endif
+#endif
+
 #include "Core/EntryPoint.hpp"
 #include "Hart.hpp"
 
 class GameLayer :public Hart::Layer {
 public:
-	GameLayer(const std::string& name = "GameLayer") 
+	GameLayer(const std::string& name = "GameLayer")
 		:Layer(name) {
 
 	}
@@ -17,6 +23,9 @@ public:
 		m_CameraController = std::make_shared<Hart::DefaultOrthographicCameraController>(640.0f, 480.0f, false);
 		// disabling camera movement
 		m_CameraController->setMovementSpeed(0.0f);
+
+		m_Font = std::make_shared<Hart::Font>("res/fonts/Roboto-Regular.ttf");
+		Hart::Renderer2D::SetFont(m_Font);
 	}
 	virtual void onDetach() override {
 
@@ -43,9 +52,37 @@ public:
 			m_PlayerTwoPos.y -= PLAYER_SPEED * deltaTime;
 		}
 
-		float halfHeight = m_PlayerSize.y / 2.0f;
-		m_PlayerOnePos.y = Hart::clamp(m_PlayerOnePos.y, -1.0f + halfHeight, 1.0f - halfHeight);
-		m_PlayerTwoPos.y = Hart::clamp(m_PlayerTwoPos.y, -1.0f + halfHeight, 1.0f - halfHeight);
+		float playerHalfHeight = m_PlayerSize.y / 2.0f;
+		m_PlayerOnePos.y = Hart::clamp(m_PlayerOnePos.y, -1.0f + playerHalfHeight, 1.0f - playerHalfHeight);
+		m_PlayerTwoPos.y = Hart::clamp(m_PlayerTwoPos.y, -1.0f + playerHalfHeight, 1.0f - playerHalfHeight);
+
+		if (m_BallPos.x < -1.333f) {
+			// player two scores
+			m_PlayerTwoScore++;
+			reset();
+		}
+		if (m_BallPos.x > 1.333f) {
+			// player one  scores
+			m_PlayerOneScore++;
+			reset();
+		}
+
+		if (m_BallPos.y < -1.0f || m_BallPos.y > 1.0f) {
+			m_BallVel.y *= -1.0f;
+		}
+
+		if (aabbCollision(m_PlayerOnePos, m_PlayerSize, m_BallPos, m_BallSize)) {
+			m_BallVel.x = -m_BallVel.x;
+			m_BallPos.x += 0.05f;
+		}
+
+		if (aabbCollision(m_PlayerTwoPos, m_PlayerSize, m_BallPos, m_BallSize)) {
+			m_BallVel.x = -m_BallVel.x;
+			m_BallPos.x -= 0.05f;
+		}
+
+		m_BallPos += Hart::Vec3::ScalarMultiply(m_BallVel, BALL_SPEED * deltaTime);
+		
 	}
 
 	virtual void render() override {
@@ -58,20 +95,44 @@ public:
 
 			Hart::Renderer2D::DrawQuad(m_BallPos, m_BallSize, Hart::White);
 
+			Hart::Renderer2D::DrawText(std::to_string(m_PlayerOneScore), { -0.5f, 0.0f, 0.0f }, 5.0f, Hart::Gray);
+			Hart::Renderer2D::DrawText(std::to_string(m_PlayerTwoScore), { 0.3f, 0.0f, 0.0f }, 5.0f, Hart::Gray);
+
 		}
 		Hart::Renderer2D::EndScene();
 	}
+
+	void reset() {
+		m_PlayerOnePos = { -1.2f, 0.0f };
+		m_PlayerTwoPos = { 1.2f, 0.0f };
+		m_BallPos = { 0.0f, 0.f };
+		m_BallVel = { 0.1f, -0.1f };
+	}
+
+	bool aabbCollision(const Hart::Vec3& objOnePos, const Hart::Vec3& objOneSize, const Hart::Vec3& objTwoPos, const Hart::Vec3& objTwoSize) {
+		return (
+			objOnePos.x + objOneSize.x > objTwoPos.x &&
+			objOnePos.x < objTwoPos.x + objTwoSize.x &&
+			objOnePos.y + objOneSize.y > objTwoPos.y &&
+			objOnePos.y < objTwoPos.y + objTwoSize.y
+			);
+	}
+
 private:
-	Hart::Vec3 m_PlayerOnePos={ -1.2f, 0.0f };
-	Hart::Vec3 m_PlayerTwoPos = { 1.2f, 0.0f };
+	Hart::Vec3 m_PlayerOnePos={ -1.2f, 0.0f, 1.0f };
+	Hart::Vec3 m_PlayerTwoPos = { 1.2f, 0.0f, 1.0f };
 	Hart::Vec2 m_PlayerSize = { 0.08f, 0.4f };
 
-	Hart::Vec3 m_BallPos = { 0.0f, 0.f };
+	Hart::Vec3 m_BallPos = { 0.0f, 0.0f, 1.0f };
 	Hart::Vec2 m_BallSize = { 0.04f, 0.04f };
+	Hart::Vec3 m_BallVel = { 0.1f, -0.1f };
 
-	std::uint32_t m_PlayerScore = 0;
+	std::shared_ptr<Hart::Font> m_Font;
+
+	std::uint32_t m_PlayerOneScore = 0;
+	std::uint32_t m_PlayerTwoScore = 0;
 	const float PLAYER_SPEED = 2.0f;
-	const float BALL_SPEED = 5.0f;
+	const float BALL_SPEED = 6.0f;
 
 	std::shared_ptr<Hart::DefaultOrthographicCameraController> m_CameraController;
 };
