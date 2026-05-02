0363da3ccf13ce0706a463226f1df6d2032b5b67
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2026-05-02 19:37:47 +0530

    fixed assert macros  - also fixed resulting issues uncovered from fixing it

--- Sandbox/src/Layer2D.cpp
+++ Sandbox/src/Layer2D.cpp
@@ -37,6 +37,8 @@ void Layer2D::onAttach() {
 
 	//Hart::AudioManager::PlaySound(pickupSound);
 	//Hart::AudioManager::PlayMusic(criticalTheme);
+
+	HART_ASSERT_EQUAL(2 + 2, 7, "wtf");
 }
 
 void Layer2D::onDetach() {
