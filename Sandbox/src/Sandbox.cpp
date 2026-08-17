b8554d34d347c84aa6580146c2bc7752e868778d
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2026-08-17 10:30:20 +0530

    disabled pch, didnt remove yet

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -19,10 +19,6 @@ public:
 		setBackgroundColor(Hart::DarkGreenishBlue);
 
 		m_Layer2D = std::make_shared<Layer2D>("Layer2D");
-
-		std::cout << Hart::Vec3::CrossProduct(Hart::Vec3(1, 0, 0), Hart::Vec3(0, 1, 0)) << '\n';
-		std::cout << Hart::Vec3::CrossProduct(Hart::Vec3(0, 1, 0), Hart::Vec3(1, 0, 0)) << '\n';
-
 		pushLayer(m_Layer2D);
 	}
 
