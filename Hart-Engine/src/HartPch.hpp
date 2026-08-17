f3647071e1335581340d3f1e21b1611cc28f6095
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2026-08-17 12:23:03 +0530

    re-enabled pch

--- Hart-Engine/src/HartPch.hpp
+++ Hart-Engine/src/HartPch.hpp
@@ -1,35 +1,49 @@
-// #pragma once
+#pragma once
 
-// // from the standard library
-// #include <iostream>
-// #include <cstring>
-// #include <cstdint>
-// #include <cstddef>
-// #include <string>
-// #include <memory>
-// #include <array>
-// #include <vector>
-// #include <initializer_list>
-// #include <deque>
-// #include <fstream>
-// #include <filesystem>
-// #include <sstream>
-// #include <unordered_map>
-// #include <algorithm>
-// #include <functional>
-// #include <chrono>
-// #include <ctime>
-// #include <iomanip>
-// #include <random>
-// #include <climits>
-// #include <numbers>
-// #include <cmath>
-// #include <cassert>
+// from the C++ standard library
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <memory>
+#include <array>
+#include <vector>
+#include <initializer_list>
+#include <deque>
+#include <fstream>
+#include <filesystem>
+#include <sstream>
+#include <unordered_map>
+#include <algorithm>
+#include <functional>
+#include <chrono>
+#include <iomanip>
+#include <random>
+#include <numbers>
+#include <numeric>
+#include <limits>
 
-// // from engine
-// #include "Core/Base.hpp"
-// #include "Core/Assert.hpp"
-// #include "Utils/FileManager.hpp"
-// #include "Utils/Logger.hpp"
-// #include "Utils/Random.hpp"
-// #include "Utils/Timer.hpp"
+// from the C standard library
+#include <cstdint>
+#include <cstddef>
+#include <cstring>
+#include <ctime>
+#include <climits>
+#include <cmath>
+#include <cassert>
+
+// platform specific
+#if defined(HART_ENGINE_PLATFORM_WINDOWS)
+	#include <windows.h>
+	#include <intrin.h>
+#elif defined(HART_ENGINE_PLATFORM_LINUX)
+	#include <unistd.h>
+	#include <signal.h>
+#endif
+
+// from engine
+#include "Core/Base.hpp"
+#include "Core/Assert.hpp"
+#include "Utils/FileManager.hpp"
+#include "Utils/Logger.hpp"
+#include "Utils/Random.hpp"
+#include "Utils/Timer.hpp"
